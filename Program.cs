49e2f25b6b1608fb55015ca4e7996af044da82aa
Anh Nguyen <anguyen@swin.edu.au>
2021-07-20 10:36:03 +1000

    Example of basic exceptions and throwing a custom exception

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CivSem2ExceptionsExamples2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Please enter an integer");
+                var input = Console.ReadLine();
+                
+                try {
+                    int num = int.Parse(input);
+
+                    if(num < 0) {
+                        throw new NegativeNumberException("Negative number was detected");
+                    }
+
+                    Console.WriteLine(num);
+
+                } catch (FormatException x) {
+                    Console.WriteLine("You entered an input that cannot be converted into an integer - " + e.Message);
+                } catch (NegativeNumberException e) {
+                    System.Console.WriteLine(e.Message);
+                } catch (Exception e) {
+                    Console.WriteLine("Something went wrong :( " + e.Message);
+                } finally {
+                    Console.WriteLine("Thanks for using this program, please come again!");
+                }           
+        }
+    }
+
+    class NegativeNumberException: Exception {
+        public NegativeNumberException(string message): base(message) { }
+    }
+}
